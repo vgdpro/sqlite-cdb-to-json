cb5533b5f6984a827a6b448012c17bfaa764b88a
jwyxym <jwyxym@126.com>
2024-09-27 02:13:14 +0800

    fix

---
+++ sqlite_cdb_to_json.py
@@ -0,0 +1,152 @@
+from cgitb import reset
+import sqlite3
+import json
+from unittest import result
+
+def sql():
+    # 连接到SQLite数据库
+    conn = sqlite3.connect('./cdb/cards.cdb')
+    cursor = conn.cursor()
+
+    # 执行SQL查询
+    cursor.execute("select * from datas,texts where datas.id=texts.id")
+    rows = cursor.fetchall()  # 获取所有行数据
+
+    # 处理数据并转换为JSON
+    data = change_db_to_json(rows)
+
+    # 转换为JSON格式并写入文件
+    with open('output.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    # 关闭数据库连接
+    conn.close()
+
+def change_db_to_json(rows):
+    data = []
+    for row in rows:
+        item = {
+            "card_name": row[13],
+            "card_id": str(row[0]),
+            "card_country": card_country( row[11], "country"),
+            "card_bloc": card_country( row[11], "bloc"),
+            "card_type": card_type( row[4] ),
+            "card_setcard": card_setcard( row[3], row[0]),
+            "card_level": (card_level_and_strike( row[7], "level")),
+            "card_skill": card_skill( row[9] ),
+            "card_trigger": card_trigger( row[8] ),
+            "card_atk": card_atk(row[5], row[10]),
+            "card_def": card_defend(row[6]),
+            "card_defender": card_defender(row[10]),
+            "card_critical_strike": card_level_and_strike( row[7], "critical_strike"),
+            "card_text": row[14]
+        }
+        data.append(item)
+    return data
+    
+def card_country(country_number, position):
+    number = str(hex(country_number)).removeprefix('0x')
+    
+    while (len(number) % 4 > 0):
+        number = '0' + number
+    
+    if (position == "bloc"):
+        if (int(number[0], 16) > 0): return str(int(number[0], 16))
+        return '-'
+        
+    result = []
+    country_table = [0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100, 0x200, 0x400, 0x800]
+    country_table_str = ['2', '4', '8', '10', '20', '40', '80', '100', '200', '400', '800']
+    
+    for i in range(len(country_table)):
+        if (country_number & country_table[i] > 0):
+            result.append(country_table_str[i])
+            
+    if (len(result) == 0):
+         return ['-']
+    
+    return result
+
+def card_type(type_number):
+    result = []
+    type_table = [0x1, 0x2, 0x4, 0x20, 0x40, 0x80, 0x200, 0x4000, 0x10000, 0x20000, 0x100000]
+    type_table_str = ['1', '2', '4', '20', '40', '80', '200', '4000', '10000', '20000', '100000']
+    
+    for i in range(len(type_table)):
+        if (type_number & type_table[i] > 0):
+            result.append(type_table_str[i])
+            
+    if (len(result) == 0):
+         return ['-']
+    
+    return result
+
+def card_setcard(setcard_number, code):
+    number = str(hex(setcard_number)).removeprefix('0x')
+    
+    while (len(number) % 4 > 0):
+        number = '0' + number
+        
+    result = []
+    a = ' '
+    
+    if (code == 20401007):
+        result.append('0090')
+    
+    for character in number:
+        if (a == ' '): a = character
+        else: a += character
+        if (len(a) == 4):
+            if (a != "0000"):
+                result.append(a)
+            a = ' '
+            
+    if (len(result) == 0):
+         return ['-']
+    
+    return result
+
+def card_atk(atk_number, atk_add):
+    result = str(atk_number)
+    if (atk_add & 0x2 > 0): result += '+'
+    
+    return result
+
+def card_defend(def_number):
+    if (def_number == -2): return '-'
+    
+    return str(def_number)
+
+def card_defender(defender_number):
+    if (defender_number & 0x1 > 0): return 'defender'
+    
+    return '-'
+
+def card_level_and_strike(l_a_s_number, position):
+    number = str(hex(l_a_s_number)).removeprefix('0x')
+    a = 0
+    
+    while (len(number) % 7 > 0):
+        number = '0' + number
+
+    if (position == "critical_strike"): return str(int(number[0], 16))
+    elif (position == "level"): return str(int(number[6], 16) - 1)
+
+def card_skill(skill_number):
+    result = []
+    skill_table = [0x2, 0x4, 0x8, 0x10, 0x20, 0x40]
+    skill_table_str = ['2', '4', '8', '10', '20', '40']
+    
+    for i in range(len(skill_table)):
+        if (skill_number & skill_table[i] > 0): result.append(skill_table_str[i])
+        
+    if (len(result) == 0):
+         return ['-']
+    
+    return result
+
+def card_trigger(trigger_number):
+    if (trigger_number == 1): return ['-']
+    return [str(trigger_number)]
+
+sql()
